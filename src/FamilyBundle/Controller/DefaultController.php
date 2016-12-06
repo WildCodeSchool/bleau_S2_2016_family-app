45825d8db075a80ce0733175390f554764af0ce4
brmat <baur.mathieu@laposte.net>
2016-12-06 16:49:22 +0100

    fix /

--- src/FamilyBundle/Controller/DefaultController.php
+++ src/FamilyBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FamilyBundle:Default:index.html.twig');
+        return $this->render('@Family/Default/index.html.twig');
     }
 
     public function phoneAction()
