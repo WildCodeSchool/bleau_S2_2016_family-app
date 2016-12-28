4cebbcaac3bccd9892b37158a5316cfeb8bd964f
brmat <baur.mathieu@laposte.net>
2016-12-28 03:42:51 +0100

    tittre

--- src/FamilyBundle/Resources/public/js/main.js
+++ src/FamilyBundle/Resources/public/js/main.js
@@ -1,19 +1,22 @@
 /**
  * Main JS
  */
-$(".button-collapse").sideNav();
+
 
 $(document).ready(function() {
     $('select').material_select();
+    // Ini Menu burger 
+    $(".button-collapse").sideNav();
+
+    $('#calendar').fullCalendar();
 });
+
+
 
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
 });
 
-$('.fixed-action-btn').openFAB();
-$('.fixed-action-btn').closeFAB();
-$('.fixed-action-btn.toolbar').openToolbar();
-$('.fixed-action-btn.toolbar').closeToolbar();
-        
+
+
