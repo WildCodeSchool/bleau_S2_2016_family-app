9950427b6e59a7100ccfb2d84168e2d9cc1e59d8
brmat <baur.mathieu@laposte.net>
2017-01-05 17:22:46 +0100

    calendar on

--- src/FamilyBundle/Resources/public/js/calendar_main.js
+++ src/FamilyBundle/Resources/public/js/calendar_main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'month,agendaWeek,agendaDay,listWeek'
+            right: 'month,basicWeek,basicDay'
         },
         defaultDate: '2016-12-12',
         navLinks: true, // can click day/week names to navigate views
